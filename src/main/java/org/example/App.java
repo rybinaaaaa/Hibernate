075012ab54a92e987cf5ed292da87eedffd619b7
rybinali <rybinaaaaa.a@gmail.com>
2023-09-27 14:51:41 +0200

    first request

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,28 @@
 package org.example;
 
+
+import org.example.Model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();){
+            Session session = sessionFactory.getCurrentSession();
+
+            session.beginTransaction();
+            Person p = session.get(Person.class, 1);
+
+            System.out.println(p);
+
+            session.getTransaction().commit();
+        }
     }
 }
