1ca08939db0c525dfcd016164d2494a6586db639
rybinali <rybinaaaaa.a@gmail.com>
2023-10-02 16:52:20 +0200

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,6 @@
 package org.example;
 
 
-import org.example.Model.Item;
-import org.example.Model.Passport;
-import org.example.Model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -13,8 +10,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Item.class).addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration();
 
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();) {
@@ -22,17 +18,6 @@ public class App {
 
             session.beginTransaction();
 
-            Person person = new Person("new test oneToOne", 99);
-
-            Passport passport = new Passport(person, 123456);
-
-//            установление связи со 2 стороны
-
-            person.setPassport(passport);
-
-// обязательно утсанрвить связь надо со стороны, где используется аннотация @JoinColumn
-
-            session.persist(person);
 
             session.getTransaction().commit();
         }
